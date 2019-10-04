aaa58c7b66a449dfe6a33b37f4a4dc68a504618f
a_wels03 <alex.welsing@web.de>
2019-10-04 12:49:55 +0200

    inital commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿using System.Windows.Forms;
+
+namespace PhotoSelect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = (AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right);
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.button2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(-4, 1);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1323, 597);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 568);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Eww";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.eww);
+            this.button1.Anchor = AnchorStyles.None;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1202, 568);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Verschwommen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.nope);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(4, 568);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 25);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Gut";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.accepted);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = (AnchorStyles.Top |AnchorStyles.Bottom|AnchorStyles.Left|AnchorStyles.Right);
+            this.pictureBox1.Location = new System.Drawing.Point(274, 1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(757, 562);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(4, 4);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 28);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Ort Wählen";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.ChoseFolder);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1318, 595);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoSelect
+{
+    public partial class Form1 : Form
+    {
+        string[] files;
+        string current;
+        string path;
+        int currentIndex;
+        string toPlace = "";
+        public Form1()
+        {
+            InitializeComponent();
+            ChoseFolder(null, null);
+            currentIndex = 0;
+            SetPicture();
+            this.pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            // this.TopMost = true;
+            this.WindowState = FormWindowState.Normal;
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            this.TopMost = true;
+        }
+
+        void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                toPlace = "accepted";
+                movePhoto(); 
+            }
+            if(e.KeyCode == Keys.Back)
+            {
+                toPlace = "declined";
+                movePhoto();
+            }
+            if(e.KeyCode == Keys.D)
+            {
+                toPlace = "komische_gesichter";
+                movePhoto();
+
+            }
+        }
+
+        private void ChoseFolder(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    path = fbd.SelectedPath;
+                    files = Directory.GetFiles(fbd.SelectedPath, "*.JPG");
+                    System.Diagnostics.Debug.Write(current);
+                    MessageBox.Show("Fotos gefunden: " + files.Length.ToString(), "Fotos gefunden", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+
+                }
+            }
+        }
+        private void movePhoto()
+        {
+            if (currentIndex >= files.Length)
+            {
+                MessageBox.Show("Keine weiteren Fotos", "Fertig", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            }
+            else
+            {
+                String[] splitted = files[currentIndex].Split('\\');
+                current = splitted[splitted.Length - 1];
+
+                String from = files[currentIndex];
+                String to = Path.Combine(path + "\\" + toPlace + "\\" + current);
+
+                Directory.CreateDirectory(path + "\\" + toPlace);
+                File.Move(from, to);
+
+                current = current.Split(new[] { '.' })[0] + ".CR2";
+
+                try
+                {
+                    from = Path.Combine(path + "\\" + current);
+                    to = Path.Combine(path + "\\" + toPlace + "\\raw\\" + current);
+                    Directory.CreateDirectory(path + "\\" + toPlace + "\\raw");
+                    File.Move(from, to);
+                }
+                catch (Exception)
+                {
+
+                }
+                currentIndex++;
+                if (currentIndex >= files.Length)
+                {
+                    MessageBox.Show("Keine weiteren Fotos", "Fertig", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                }
+                else
+                {
+                    SetPicture();
+                }
+            }
+        }
+
+        private void SetPicture()
+        {
+            if (currentIndex < files.Length)
+            {
+                using (FileStream fileStream = new FileStream(files[currentIndex], FileMode.Open))
+                {
+                    pictureBox1.Image = new Bitmap(fileStream);
+                    pictureBox1.Update();
+
+                }
+            }
+
+        }
+        private void eww(object sender, EventArgs e)
+        {
+            toPlace = "komische_gesichter";
+            movePhoto();
+        }
+        private void accepted(object sender, EventArgs e)
+        {
+            toPlace = "accepted";
+            movePhoto();
+        }
+        private void nope(object sender, EventArgs e)
+        {
+            toPlace = "declined";
+            movePhoto();
+        }
+
+    }
+}
